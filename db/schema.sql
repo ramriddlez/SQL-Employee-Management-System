185bcbc235545317a27137b9d958c98ab08157f5
ramriddlez <ramankangz@outlook.com>
2022-02-27 22:26:19 -0500

    changes

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE employees_db;
 
 CREATE TABLE departments (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    fname VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
@@ -21,5 +21,5 @@ CREATE TABLE employees(
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL, FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
-    manager_id INT VARCHAR(30) NOT NULL, FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE CASCADE,
+    manager_id INT, FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE CASCADE,
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,22 @@
-INSERT INTO
+INSERT INTO departments (id, fname)
+VALUES (1, "Engineering"), (2, "Finance"), (3, "Legal"), (4, "Sales");
+
+INSERT INTO roles (title, department_id, salary)
+VALUES ("Sales Lead", 1, 100000), 
+       ("Salesperson", 1, 80000),
+       ("Lead Engineer", 2, 150000), 
+       ("Software Engineer", 2, 120000),
+       ("Account Manager", 3, 160000), 
+       ("Accountant", 3, 125000),
+       ("Legal Team Lead", 4, 250000), 
+       ("Lawyer", 4, 190000);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+Values ("John", "Doe", 1, 3),
+       ("Mike", "Chan", 2, 1),
+       ("Ashley", "Rodriguez", 3, 3),
+       ("Kevin", "Tupik", 4, 3),
+       ("Kunal", "Singh", 5, 2),
+       ("Malia", "Brown", 6, 5),
+       ("Sarah", "Lourd", 7, 1),
+       ("Tom", "Allen", 8, 7);
