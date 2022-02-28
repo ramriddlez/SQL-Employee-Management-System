f9b096b564971698e098ab21122b68c28db65868
ramriddlez <ramankangz@outlook.com>
2022-02-27 20:08:30 -0500

    adding schema db and tables;

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,20 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    
+
+)
+
+
