e8825519c1a77f091b7a4096bfa8deab95d1ab08
ramriddlez <ramankangz@outlook.com>
2022-02-28 00:17:39 -0500

    added connection root

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
 const consoletable = require("console.table");
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      // TO DO: Enter your MySQL password below
+      password: '',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
