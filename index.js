68e6b4c67f984262473103508163071c5d811eca
ramriddlez <ramankangz@outlook.com>
2022-02-28 01:09:19 -0500

    created first inquirer prompt w return function

--- index.js
+++ index.js
@@ -13,20 +13,6 @@ const db = mysql.createConnection(
     console.log(`Successfully connected to the employees database!`)
   );
 
-const options = [
-    {
-    type: 'list',
-    message: 'What would you like to do?',
-    name: 'choice',
-    choices: ["View All Employees",
-    "Add an Employee",
-    "Update an Employee Role",
-    "View All Roles",
-    "Add a Role",
-    "View All Departments",
-    "Add a Department",]
-  },
-]
 
 function init () {
     inquirer.prompt([
@@ -43,8 +29,16 @@ function init () {
             "Add a Department"]
         }
     ]).then(function (data) {
-        
-    })
+        if (data.choice === "View All Employees") {
+            db.query(
+                "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;" , (err, results) => {
+                    if (err) {
+                        console.log(err);
+                      }
+                      console.table(results);
+                })
+        }});
+
 }
 
 init();
