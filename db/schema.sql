c12df450062347ba310bacb405dfe133c7069af0
ramriddlez <ramankangz@outlook.com>
2022-02-27 20:30:05 -0500

    completed schema

--- db/schema.sql
+++ db/schema.sql
@@ -13,8 +13,13 @@ CREATE TABLE roles (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT NOT NULL,
-    
+  FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
 
-)
-
-
+CREATE TABLE employees(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL, FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    manager_id INT VARCHAR(30) NOT NULL, FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE CASCADE,
+);
